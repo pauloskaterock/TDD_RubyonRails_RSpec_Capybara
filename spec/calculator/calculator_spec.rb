887d9a8b7d9a14d591b6e10b7e26798cd238411a
ph <pauloengenharia5@yahoo.com>
2024-04-18 18:23:13 -0300

    update

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -1,11 +1,31 @@
 require '/mnt/c/TDD_RubyonRails_RSpec_Capybara/spec/calculator/calculator.rb'
 
 
-    describe Calculator do
-      it 'returns the sum of two numbers' do
-        calc = Calculator.new
-        resul = calc.sum(5,7)
-        expect(resul).to eq(12)
-       
-      end
+describe Calculator, "Sobre a Calculadora" do
+
+  context '#div' do
+    it 'divide by 0' do
+      expect{subject.div(3, 0)}.to raise_error(ZeroDivisionError)
+      expect{subject.div(3, 0)}.to raise_error("divided by 0")
+      expect{subject.div(3, 0)}.to raise_error(ZeroDivisionError, "divided by 0")
+      expect{subject.div(3, 0)}.to raise_error(/divided/)
     end
+  end
+
+  context '#sum' do
+    it 'with positive numbers' do
+      result = subject.sum(5, 7)
+      expect(result).to eq(12)
+    end
+
+    it 'with negative and positive numbers' do
+      result = subject.sum(-5, 7)
+      expect(result).to eq(2)
+    end
+
+    it 'with negative numbers' do
+      result = subject.sum(-5, -7)
+      expect(result).to eq(-12)
+    end
+  end
+end
